995d4a40ca4681ad28d4db4bccd537cfce2cea2d
Yan Carlos <78356819+K0dax@users.noreply.github.com>
2022-09-14 13:52:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Tomar café", "Aulas de node"];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("pt-BR", options);
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  items.push(req.body.newItem);
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
